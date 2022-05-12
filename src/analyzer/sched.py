eee32fff751adfcff517a33fa258890eb38a5044
huunguyen95 <huu.nqh@gmail.com>
2022-05-12 08:31:03 +0700

    Add class socket server

---
+++ src/analyzer/sched.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+'''
+This is model that control all the system at IoT Analyzer. Call the model and run code.
+1. Check task assign
+2. Check system state
+3. Request offload
+4. Transfer file
+5. Receive predict result
+6. Send to Dashboard
+'''
+
+
--- src/analyzer/socket_server.py
+++ src/analyzer/socket_server.py
@@ -2,6 +2,102 @@ import os
 import socket
 import threading
 
+'''
+This module have responsibility:
+1. Receive raw data from IoT Agent
+2. Send result to dashboard
+'''
+
+
+class SocketServer:
+    def __init__(self,
+                 server_ip="127.0.0.1",
+                 server_port=4456):
+        super().__init__()
+        self.addr = (server_ip, server_port)
+        self.size = 4096
+        self.format = "utf-8"
+        self.server_data_path = "server_data/test_dataset"
+        self.task_state = False
+        print("[STARTING] Server is starting")
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.addr)
+        self.server.listen()
+        print(f"[LISTENING] Server is listening on {server_ip}:{server_port}.")
+
+    def active_socket_server(self):
+        while True:
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+    def handle_client(self, conn, addr):
+        print(f"[NEW CONNECTION] {addr} connected.")
+        conn.send("OK@Welcome to the File Server.".encode(self.format))
+        name = conn.recv(self.size).decode(self.format)
+        print(f"[RECEIVING] {name}")
+
+        conn.send("[RECEIVED] FILE NAME".encode(self.format))
+        name = name.split("/")[-1]
+        filepath = os.path.join(self.server_data_path, name)
+
+        with open(filepath, "w") as f:
+            while True:
+                text = conn.recv(self.size).decode(self.format )
+                # print(text)
+                f.write(text)
+                if not text:
+                    break
+
+        f.close()
+        self.task_state = True
+        '''
+        CALL THE information-stategy model for event task assignment
+        
+        '''
+
+        print(f"[DISCONNECTED] {addr} disconnected")
+        conn.close()
+        print(f"[TASK] {name}\n")
+
+    def get_task_state(self):
+        return self.task_state
+
+    def send_result(self, predict_result="benign", dashboard_ip="192.168.2.10", dashboard_port=4456):
+        self.task_state = False
+        dashboard_addr = (dashboard_ip, dashboard_port)
+
+        dataset = ["./client_data/scg/data/malware/", "./client_data/scg/data/benign/",
+                   "./client_data/scg/data/new_malware/"]
+
+        # client_data = "./client_data/scg/data/malware/"
+
+        def main():
+            for client_data in dataset:
+                for filename in os.listdir(client_data):
+                    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    client.connect(ADDR)
+                    data = client.recv(SIZE).decode(FORMAT)
+                    print(data)
+                    f = os.path.join(client_data, filename)
+                    if os.path.isfile(f):
+                        print("Uploading: ", f)
+                        with open(f"{f}", "r") as file:
+                            text = file.read()
+                    send_filename = f"{f}"
+                    client.send(send_filename.encode(FORMAT))
+                    print(f"{client.recv(SIZE).decode(FORMAT)}")
+                    send_data = f"{text}"
+                    # print(send_data)
+                    client.sendall(send_data.encode(FORMAT))
+                    # ack1 = client.recv(SIZE).decode(FORMAT)
+                    # print(f"Server ACK1: {ack1}")
+                    print(f"UPLOADED: {filename}")
+                    client.close()
+
+
+'''
 #IP = socket.gethostbyname(socket.gethostname())
 IP = "192.168.2.10"
 PORT = 4456
@@ -59,3 +155,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
